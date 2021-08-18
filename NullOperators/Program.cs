a6d8fa5b856a1124937d50f3af831f51e3db2685
carmelhl <carmelhl@gmail.com>
2021-08-18 14:38:31 +1000

    creation
    creation

---
+++ NullOperators/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace NullOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var line1 = "line 1";
+            string line2 = null;
+            var line3 = "";
+
+
+
+            Console.WriteLine("line1: {0} ", line1 ?? "line1 evaluted null");
+            Console.WriteLine("line2: {0} ", line2 ?? "line2 evaluted null");
+            Console.WriteLine("line3: {0} ", line3 ?? "line3 evaluted null");
+
+
+            var b = new a();
+
+            Console.WriteLine("b.val: {0} ", b?.val);
+
+            b = null;
+
+            Console.WriteLine("b.val: {0} ", b?.val);
+
+        }
+
+
+    }
+
+    class a 
+    {
+        public int val { get; set; }
+    }
+}
